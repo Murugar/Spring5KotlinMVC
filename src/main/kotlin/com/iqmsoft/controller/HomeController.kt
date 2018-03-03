ac491239f32ce40228eda1ad71091e9e0ac2ee7d
Davanon <davanon2014@gmail.com>
2018-03-03 12:21:45 +0530

    First

---
+++ src/main/kotlin/com/iqmsoft/controller/HomeController.kt
@@ -0,0 +1,12 @@
+package com.iqmsoft.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+class HomeController {
+    @RequestMapping(value = "/")
+    fun home() : String {
+        return "home"
+    }
+}
